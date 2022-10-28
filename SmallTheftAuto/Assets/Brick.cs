986feaecf12c33a949f371a0b2f4c9a6a6a8da5f
philipschelin02 <turzeke@gmail.com>
2022-10-28 14:14:15 +0200

    Completed 2.3 - Event Functions (Drag)

--- SmallTheftAuto/Assets/Brick.cs
+++ SmallTheftAuto/Assets/Brick.cs
@@ -1,12 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Brick : MonoBehaviour
+public class Brick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     // Start is called before the first frame update
     void Start()
     {
         transform.position += transform.right * Random.Range(-.1f, .1f);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Plane plane = new Plane(Vector3.up, Vector3.up * eventData.pointerPressRaycast.worldPosition.y);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out float distance))
+        {
+            transform.position = ray.GetPoint(distance);
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        GetComponent<Rigidbody>().isKinematic = false;
     }
 }
