1814362d30b68d4261ab611576dcfaf9d356dfa1
James Lind <112474995+Jlindhub@users.noreply.github.com>
2022-11-08 16:57:52 +0100

    disappearing fire
    the fire works now this is pain

--- SmallTheftAuto/Assets/Scripts/Fire.cs
+++ SmallTheftAuto/Assets/Scripts/Fire.cs
@@ -7,9 +7,14 @@ public class Fire : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(selfdestruct());
     }
+
     IEnumerator selfdestruct()
+    {
+        yield return new WaitForSeconds(5f);
+        Destroy(gameObject);
+    }
     // Update is called once per frame
     void Update()
     {
