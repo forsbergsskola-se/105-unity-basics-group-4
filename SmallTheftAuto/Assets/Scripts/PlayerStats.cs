744fbd3863e5b28a6f73778d1b4b3c34b027a1c3
Leo S <leo.e.sever@gmail.com>
2022-11-08 23:47:19 +0100

    Changed health text into a working healthbar

--- SmallTheftAuto/Assets/Scripts/PlayerStats.cs
+++ SmallTheftAuto/Assets/Scripts/PlayerStats.cs
@@ -8,18 +8,31 @@ using UnityEngine.Events;
 public class PlayerStats : MonoBehaviour, ITakeDamage
 {
     
-    public TMP_Text text; 
-    int health = 100;
+    public TMP_Text text;
+    public int maxhealth = 100;
+    public int currentHealth;
+    public HealthBar healthBar;
+    public int money = 9999;
     public int CarsDestroyed;
+
+    public void Start()
+    {
+        currentHealth = maxhealth;
+        healthBar.SetMaxHealth(maxhealth);
+    }
 
     private void Update()
     {
-        text.SetText(Convert.ToString($"Health:{health}"));
-}
-
+        text.SetText(Convert.ToString($"${money}"));
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            takedamage(10);
+        }
+    }
     public void takedamage(int damagedealt)
     {
-        health = -damagedealt;
+        currentHealth -= damagedealt;
+        healthBar.SetHealth(currentHealth);
     }
 
     public void CarDestroyed()
---
+++ SmallTheftAuto/Assets/Scripts/UIScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Healthbar : MonoBehaviour
+{
+    private Image healthBar;
+    public float currentHealth;
+    private float maxHealth = 100f;
+
+}
