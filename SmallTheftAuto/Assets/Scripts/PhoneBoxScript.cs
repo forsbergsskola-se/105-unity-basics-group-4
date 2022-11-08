8672c661d744c088f7a033d45e30e6952da1d518
Bapmus <hampus.brink@elev.ga.lbs.se>
2022-11-08 13:51:06 +0100

    Added phonebox + buttons + scripts

--- SmallTheftAuto/Assets/Scripts/PhoneBoxScript.cs
+++ SmallTheftAuto/Assets/Scripts/PhoneBoxScript.cs
@@ -12,6 +12,7 @@ public class PhoneBoxScript : MonoBehaviour
     public Transform player;
     public int phoneDistance;
     public GameObject buttonHolder;
+    private Quest quest;
 
     private string questName;
     void Start()
@@ -24,28 +25,94 @@ public class PhoneBoxScript : MonoBehaviour
         if (Vector3.Distance(transform.position, player.position) < phoneDistance && Input.GetKeyDown(KeyCode.F))
         {
               //interaction logic here.
-              GetQuest();
+              buttonHolder.SetActive(true);
+              if (quest?.ClearConditon() is not false)
+              {
+                  quest = GetQuest();
+                  TMP_Text[] buttonDisplay = buttonHolder.GetComponentsInChildren<TMP_Text>();
+                  buttonDisplay[0].SetText(quest.name);
+              }
+              
+        }
+
+        if (quest.ClearConditon())
+        {
+            //Logic when quest is completed. rewards,xp,money.etc
         }
     }
 
-    void GetQuest()
-    {
-        buttonHolder.SetActive(true);
-        int random = Random.Range(1, 4);
-        
+    Quest GetQuest()
+    { 
+        int random = Random.Range(1, 5);
+        if (random == 1)
+        {
+            return new KillingQuest();
+        }
+        if (random == 2)
+        {
+            return new KillingQuest();
+        }
+        if (random == 3)
+        {
+            return new KillingQuest();
+
+        }
+        if (random == 4)
+        {
+            return new KillingQuest();
+
+        }
+
+
+
+
+        return null;
 
     }
 
     public void SelectQuest(Button button)
     {
         buttonHolder.SetActive(false);
-        button.GetComponentInChildren<TMP_Text>().SetText($""); 
     }
 
 }
-
-public class Quest
+public interface IClearable
 {
-    private string _name;
-    private int _reward;
+    public bool ClearConditon();
 }
+
+public abstract class Quest : IClearable
+{
+    public string name;
+    public int reward = 2;
+    void KillingQuest()
+    {
+        
+    }
+
+
+    public abstract bool ClearConditon();
+}
+
+public class KillingQuest : Quest, IClearable
+{
+    public int victims = 3;
+
+
+    public KillingQuest()
+    {
+        name = $"Kill {victims} people to earn {reward} money.";
+        reward = 49;
+    }
+    
+    public override bool ClearConditon()
+    {
+        if (victims == 3)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
+
