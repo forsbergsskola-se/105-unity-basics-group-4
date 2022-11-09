99a6339b06e122631976cd88d26c26cc78e86045
James Lind <112474995+Jlindhub@users.noreply.github.com>
2022-11-09 05:51:05 +0100

    oof euch hot fire
    adds damage from touching or going too close to fires.

--- SmallTheftAuto/Assets/Scripts/Fire.cs
+++ SmallTheftAuto/Assets/Scripts/Fire.cs
@@ -1,10 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
     public float firetimer;
+    public bool playertriggering;
+    public bool burningplayer;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +19,38 @@ public class Fire : MonoBehaviour
     {
         yield return new WaitForSeconds(firetimer);
         Destroy(gameObject);
+    }
+    public void OnTriggerStay(Collider other)
+    {
+        PlayerStats playerStats = FindObjectOfType<PlayerStats>();
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playertriggering = true;
+            if (!burningplayer) //ensures that only one instance of burningdamage is started and active.
+            {
+                StartCoroutine(Burningdamage(playerStats));
+            }
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playertriggering = false;
+        }
+    }
+
+    IEnumerator Burningdamage(PlayerStats playertoburn)
+    {
+        burningplayer = true;
+        while (playertriggering)
+        {
+            playertoburn.takedamage(10);
+            yield return new WaitForSecondsRealtime(1f);
+        }
+
+        burningplayer = false;
     }
     // Update is called once per frame
 }
