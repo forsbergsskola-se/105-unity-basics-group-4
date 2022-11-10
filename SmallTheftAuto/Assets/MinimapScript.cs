922009094e7c6ddd8b97122a112aacf70e4c22e5
Leo Sever <leo.e.sever@gmail.com>
2022-11-10 15:10:57 +0100

    added flip feature to car

--- SmallTheftAuto/Assets/MinimapScript.cs
+++ SmallTheftAuto/Assets/MinimapScript.cs
@@ -1,18 +1,34 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class MinimapScript : MonoBehaviour
+public class MinimapScript: MonoBehaviour
 {
- public Transform player;
+    private GameObject objectFollow;
+    public GameObject player;
+    public Vector3 offset;
+    private Vector3 velocity;
+    
+    void LateUpdate()
+    {
+        var position = objectFollow.transform.position;
+        transform.position = position+offset;
 
- private void LateUpdate()
- {
-     Vector3 newPosition = player.position;
-     newPosition.y = transform.position.y;
-     transform.position = newPosition;
-     
-     
- }
+    }
+
+    void Update()
+    {
+        if (!player.activeInHierarchy)
+        {
+            objectFollow = FindObjectOfType<CarMovementScript>().gameObject;
+
+        }
+        else
+        {
+            objectFollow = FindObjectOfType<PlayerMovement>().GameObject();
+
+        }
+    }
 }
